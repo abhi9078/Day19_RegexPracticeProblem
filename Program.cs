f48a94da4951ab98b57d1d0096e9b9bebed89468
Abhilash <abhilashmeher1234@gmail.com>
2022-03-30 13:21:41 +0530

    Added program file for UC_1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("Please Enter your First name");
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][a-z]{2,}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("your First name is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your First name is " + false);
+                    Console.WriteLine("First Name Should Contain Only Four Character");
+                }
+            }
+        }
+    }
+}
